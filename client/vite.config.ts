431c4158ca63b6880ad5e2aba847609d8ab11c5d
chrishanthacosta <chrishanthacosta@gmail.com>
2025-08-21 19:24:39 +0530

    remove port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,9 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173, // Change this to your desired port
-  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
